0cc9943c2626362d991f36f5d7341fc47eb5842a
oguzhandemirkaya <ogsandemirkaya@gmail.com>
2024-05-13 02:17:47 +0300

    dsd

--- app/page.tsx
+++ app/page.tsx
@@ -70,9 +70,7 @@ export default function Home() {
 
   return (
     <>
-  <Head>
-  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-</Head>
+  
     <div>
     
     <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
